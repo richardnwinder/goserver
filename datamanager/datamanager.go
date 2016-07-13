89dd5cb4f5c8f7a1f61f9b32764c3cc388fc63e5
richardnwinder <devel2go@gmail.com>
2016-07-13 16:12:52 +0100

    Add files via upload

---
+++ datamanager/datamanager.go
@@ -0,0 +1,13 @@
+// package datamanager contains routines for querying databases
+package datamanager
+
+import (
+	_ "fmt"
+
+	_ "github.com/patrickjuchli/couch"
+)
+
+func ClubIndex(user string, country string, club string, language string) string {
+	html := "Html"
+	return html
+}
